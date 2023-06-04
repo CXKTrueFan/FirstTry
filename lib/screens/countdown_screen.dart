f56a9a7ab253e8276fde2d08a2c7fd52c0af1f60
TAO WU <wutao@mail.ynu.edu.cn>
2023-06-04 07:38:14 +0000

    SAVE

---
+++ lib/screens/countdown_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class CountdownScreen extends StatefulWidget {
+  @override
+  _CountdownScreenState createState() => _CountdownScreenState();
+}
+
+class _CountdownScreenState extends State<CountdownScreen> {
+  int _seconds = 10;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startCountdown();
+  }
+
+  void _startCountdown() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_seconds > 0) {
+          _seconds--;
+        } else {
+          _timer.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('倒计时十秒'),
+      ),
+      body: Center(
+        child: Text(
+          '$_seconds',
+          style: TextStyle(fontSize: 48.0),
+        ),
+      ),
+    );
+  }
+}
